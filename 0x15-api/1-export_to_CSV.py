9e73a7cbe4db83c8f1c14e7e09b5ed783ec14ced
Sami Lafine <lafine.sami@gmail.com>
2020-05-05 20:08:11 +0000

    export to csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -6,44 +6,30 @@ import json
 import sys
 
 
-def gather_data():
-    ''' gathers data from the api '''
-    try:
-        userid = sys.argv[1]
-    except:
-        print('Usage: ./0-gather_data_from_an_API.py <employee ID>')
-        return
-
-    userurl = 'https://jsonplaceholder.typicode.com/users'
-
-    t = requests.get(userurl+'/'+userid+'/todos')
-    u = requests.get(userurl+'/'+userid)
-
-    task_count = 0
-    for task in t.json():
-        task_count += 1
-        for k, v in task.items():
-            if k == 'completed' and v is True:
-                if 'completed_task_count' not in locals():
-                    completed_task_count = 0
-                completed_task_count += 1
-
-    user_dict = u.json()
-    try:
-        emp_name = (user_dict['name'])
-    except:
-        print("Employee does not exist.")
-        return
-
-    print(
-        'Employee {} is done with tasks({}/{}):'.format(
-            emp_name, completed_task_count, task_count))
-
-    for task in t.json():
-        task_dict = task
-        if task_dict['completed'] is True:
-            print('\t {}'.format(task_dict['title']))
-
-
-if __name__ == '__main__':
-    gather_data()
+if __name__ == "__main__":
+    numbertask = 0
+    taskcomplete = 0
+    tasksdone = []
+    userid = sys.argv[1]
+    url1 = 'https://jsonplaceholder.typicode.com/todos'
+    with urllib.request.urlopen(url1) as response:
+        html = response.read()
+    dattod = json.loads(html.decode('utf-8'))
+    with urllib.request.urlopen('https://jsonplaceholder.typicode.com/users/' +
+                                sys.argv[1]) as response:
+        html = response.read()
+    datuser = json.loads(html.decode('utf-8'))
+    for item in dattod:
+        if int(item['userId']) == int(userid):
+            tasksdone.append(item)
+    file1 = "{}.csv".format(userid)
+    with open(file1, mode='w') as emplo_file:
+        employee_writer = csv.writer(
+            emplo_file,
+            delimiter=',',
+            quotechar='"',
+            quoting=csv.QUOTE_NONNUMERIC)
+        for taks in tasksdone:
+            employee_writer.writerow([str(datuser['id']),
+                                      datuser['username'],
+                                      str(taks['completed']), taks['title']])
