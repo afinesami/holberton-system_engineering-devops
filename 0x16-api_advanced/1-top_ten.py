58c88239b7e363e88b704e8d6424b26e9346d949
afinesami <lafine.sami@gmail.com>
2020-05-07 19:44:47 +0000

    top ten

---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" module for function to return top 10 hot posts of a given subreddit """
+import requests
+import sys
+
+
+def top_ten(subreddit):
+    " Args:
+        subreddit: subreddit name
+    Returns:
+        top ten post titles
+        or None if queried subreddit is invalid "
+    headers = {'User-Agent': 'xica369'}
+    url = "https://www.reddit.com/r/{}/hot.json".format(subreddit)
+    parameters = {'limit': 10}
+    response = requests.get(url, headers=headers, allow_redirects=False,
+                            params=parameters)
+
+    if response.status_code == 200:
+        titles_ = response.json().get('data').get('children')
+        for title_ in titles_:
+            print(title_.get('data').get('title'))
+    else:
+        print(None)
