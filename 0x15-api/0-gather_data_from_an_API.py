d2f0319f62dda585fb863b252b6754db303fdbad
afinesami <samibenomar84@gmail.com>
2020-05-13 09:51:23 +0000

    gather data from an api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,47 +1,21 @@
 #!/usr/bin/python3
 # Gather data from an api
+
 import requests
 import sys
-
-
-def gather_data():
-    ''' gathers data from the api '''
-    try:
-        userid = sys.argv[1]
-    except:
-        print('Usage: ./0-gather_data_from_an_API.py <employee ID>')
-        return
-
-    userurl = 'https://jsonplaceholder.typicode.com/users'
-
-    t = requests.get(userurl+'/'+userid+'/todos')
-    u = requests.get(userurl+'/'+userid)
-
-    task_count = 0
-    for task in t.json():
-        task_count += 1
-        for k, v in task.items():
-            if k == 'completed' and v is True:
-                if 'completed_task_count' not in locals():
-                    completed_task_count = 0
-                completed_task_count += 1
-
-    user_dict = u.json()
-    try:
-        emp_name = (user_dict['name'])
-    except:
-        print("Employee does not exist.")
-        return
-
-    print(
-        'Employee {} is done with tasks({}/{}):'.format(
-            emp_name, completed_task_count, task_count))
-
-    for task in t.json():
-        task_dict = task
-        if task_dict['completed'] is True:
-            print('\t {}'.format(task_dict['title']))
+import argv
 
 
 if __name__ == '__main__':
-    gather_data()
+    user_id = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(user_id)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(
+        user_id)
+    todo = requests.get(url, verify=False).json()
+    completed_tasks = [task.get("title")
+                       for task in todo if task.get("completed")]
+    print("Employee {} is done with tasks({}/{}):".
+          format(user.get('name'), len(completed_tasks), len(todo)))
+    for task in completed_tasks:
+        print("\t {}".format(task))
