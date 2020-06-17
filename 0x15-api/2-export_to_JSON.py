78abcee4186299ce0534896ff29e77914042ff2e
afinesami <samibenomar84@gmail.com>
2020-06-17 13:02:40 +0000

    2-export_to_JSON.py

--- 0x15-api/2-export_to_JSON.py
+++ 0x15-api/2-export_to_JSON.py
@@ -1,52 +1,23 @@
 #!/usr/bin/python3
-# exports data in the JSON format
+'''
+data in the JSON format
+'''
+
 import json
 import requests
-import sys
-from collections import OrderedDict
-
-
-def gather_data():
-    ''' retrieves data from the api '''
-    userurl = 'https://jsonplaceholder.typicode.com/users'
-
-    t = requests.get(userurl+'/'+userid+'/todos')
-    u = requests.get(userurl+'/'+userid)
-
-    user_dict = u.json()
-    try:
-        emp_name = (user_dict['username'])
-    except:
-        print("Employee does not exist.")
-        return
-
-    task_dict = {}
-    task_vals = []
-
-    for i in t.json():
-        std = OrderedDict()
-        std['task'] = i['title']
-        std['completed'] = i['completed']
-        std['username'] = emp_name
-        task_vals.append(std)
-
-    task_dict[str(userid)] = task_vals
-
-    return task_dict
-
-
-def save_to_json(data, userid):
-    ''' saves data to JSON '''
-    f = str(userid)+'.json'
-    with open(f, mode='w', encoding="UTF8") as fd:
-        json.dump(data, fd)
-
+from sys import argv
 
 if __name__ == '__main__':
-    try:
-        userid = sys.argv[1]
-    except:
-        raise NameError('Usage: ./2-export_to_JSON.py <employee ID>')
-
-    task_dict = gather_data()
-    save_to_json(task_dict, userid)
+    uid = argv[1]
+    url = "https://jsonplaceholder.typicode.com/users/{}".format(uid)
+    user = requests.get(url, verify=False).json()
+    url = "https://jsonplaceholder.typicode.com/todos?userId={}".format(uid)
+    todo = requests.get(url, verify=False).json()
+    name = user.get('username')
+    t = [{"task": t.get("title"),
+          "username": name,
+          "completed": t.get("completed")} for t in todo]
+    bj = {}
+    bj[uid] = t
+    with open("{}.json".format(uid), 'w') as filejs:
+        json.dump(bj, filejs)
